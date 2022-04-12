6a238caff1ffadc4ca5bad835250a6aa72457b6e
coding-to-music <connors.tom@gmail.com>
2022-04-12 04:49:05 +0000

    Heroku

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,20 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
-    })
+    });
 
     console.log(
       `MongoDB connected: ${conn.connection.host}`.green.underline.bold
-    )
+    );
   } catch (err) {
-    console.error(`💥💥💥 Error: ${err.message}`.red.bold)
-    process.exit(1)
+    console.error(`💥💥💥 Error: ${err.message}`.red.bold);
+    process.exit(1);
   }
-}
+};
 
-export default connectDB
+export default connectDB;
